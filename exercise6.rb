7ef659c4f9a30ba7e8a247d30805b8261cbb0542
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-03 13:52:02 -0400

    finished exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,42 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+puts "\nStep 1:\n\n"
 
-grocery_list.each {|item| puts '*' + item}
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list.each {|item| puts '* ' + item}
+
+puts "\nStep 2:\n\n"
+
+grocery_list.push("rice")
+
+def print_list(list)
+	list.each {|item| puts '* ' + item}
+end
+
+print_list(grocery_list)
+
+puts "\nStep 3:\n\n"
+
+puts grocery_list.size
+
+puts "\nStep 4:\n\n"
+
+if grocery_list.include?("bananas") 
+	puts "You need to pick up bananas" 
+else
+	puts "You don't need to pick up bananas"
+end
+
+puts "\nStep 5:\n\n"
+
+puts grocery_list[1]
+
+puts "\nStep 6:\n\n"
+
+print_list(grocery_list.sort)
+
+puts "\nStep 7:\n\n"
+
+grocery_list.delete("salmon")
+print_list(grocery_list)
+
+
+	
